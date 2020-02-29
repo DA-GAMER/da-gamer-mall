258b313324182f291bc03c91a6b80e83d5e0e29b
luotaodadi <ltmoker@gitlab.com>
2020-02-29 12:36:27 +0800

    排除.idea文件夹

---
+++ dagamer-mall-order/src/main/java/com/dagamer/mall/order/DagamerMallOrderApplication.java
@@ -0,0 +1,9 @@
+package com.dagamer.mall.order;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class DagamerMallOrderApplication {
+}
